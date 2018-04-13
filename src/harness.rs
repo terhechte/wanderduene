a419a931a2d9127bf85881a06901261c54beae8d
Benedikt Terhechte <terhechte@gmail.com>
2018-04-13 21:55:14 +0200

    code cleanup

--- src/harness.rs
+++ src/harness.rs
@@ -3,40 +3,6 @@ use std::io;
 use std::path::PathBuf;
 use std::collections::HashMap;
 
-/*struct TestPage;
-
-impl DunePage for TestPage {
-}
-
-
-#[test]
-fn testing() {
-    let posts = vec![OrgPost::new("test1".to_owned(), "2017".to_owned(), "05".to_owned(), "04".to_owned()),
-                     OrgPost::new("test1".to_owned(), "2016".to_owned(), "05".to_owned(), "04".to_owned()),
-                     OrgPost::new("test1".to_owned(), "2015".to_owned(), "05".to_owned(), "04".to_owned()),
-    ];
-    let pages = vec![TestPage {}];
-    let dune = DuneBase::new(posts, pages);
-    dune.posts().map(|post|println!("post: {}", post.title()));
-}
-
-
-tasks:
-- should I slice everything into the groups? i.e. instead of &Vec<BlogPost> I do &[BlogPost] ?
-*/
-
-/*struct BlogPost {
-    title: String,
-}
-
-impl BlogPost {
-    fn new() -> BlogPost {
-        BlogPost {
-            title: "yeah".to_string(),
-        }
-    }
-}*/
-
 pub struct BlogPost {
     pub identifier: String,
     pub path: String,
@@ -196,15 +162,7 @@ impl<'a> DuneBuildWriter for Builder<'a> {
         println!("writing overview at {:?}", &path);
         Ok(())
     }
-    /*fn write_posts(self) -> io::Result<()> {
-        for post in self.payload {
-            let mut path = self.path.clone();
-            path.push(&post.identifier);
-            path.push("index.html");
-            println!("writing posts at {:?}", &path);
-        }
-        Ok(())
-    }*/
+
     fn write_index(self) -> io::Result<()> {
         println!("writing index at {:?}", &self.path);
         Ok(())
@@ -242,23 +200,7 @@ impl<'a> DuneBuildWriter for GroupedDuneBuilder<'a> {
     }
 }
 
-/*impl<'a> DuneBuildCollector<'a> for GroupedDuneBuilder<'a> {
-    fn with_posts<F>(self, action: F) -> Self where F: (Fn(&'a BlogPost, i32, i32, &PathBuf) -> ()) {
-        let count = self.payload.len();
-        for (current, post) in self.payload.iter().enumerate() {
-            action(post, current, count, &self.path);
-        }
-        self
-    }
-}*/
-
 struct PagedDuneBuilder;
-
-/*impl PagedDuneBuilder {
-    fn with<F>(self, action: F) -> Self where F: (Fn(Builder, i32) -> Builder) {
-        self
-    }
-}*/
 
 #[test]
 fn testing() {
@@ -281,101 +223,3 @@ fn testing() {
                 }).write_overview().unwrap();
         }).write_overview().unwrap();
 }
-
-
-/*
-trait DuneBuilder {
-    fn group_by(self, key: DuneBaseAggType) -> Self;
-    fn paged(self) -> PageDuneBuilder;
-    // writes index.html files with short one line descs
-    fn write_overview(self) -> Self;
-    // writes folders with the name of the post and and index.html
-    fn write_posts(self) -> Self;
-    // writes an index
-    fn write_index(self) -> Self;
-}
-
-trait DuneBuilderConsumable {
-    fn consume(self) -> Result<(), Box<Error>>;
-}
-
-struct Builder<'a> {
-    payload: Cell<&'a Vec<BlogPost>>
-}
-
-impl<'a> Builder<'a> {
-    fn new(posts: &'a Vec<BlogPost>) -> Builder {
-        Builder {
-            payload: Cell::new(posts),
-        }
-    }
-}
-
-impl<'a> DuneBuilder for Builder<'a> {
-    fn group_by(self, key: DuneBaseAggType) -> Self {
-        self
-    }
-    fn paged(self) -> PageDuneBuilder {
-        PageDuneBuilder {}
-    }
-    fn overview(self) -> Self {
-        self
-    }
-    fn posts(self) -> Self {
-        self
-    }
-}
-
-impl<'a> DuneBuilderConsumable for Builder<'a> {
-    fn consume(self) -> Result<(), Box<Error>> {
-        Ok(())
-    }
-}
-
-struct PageDuneBuilder;
-impl DuneBuilder for PageDuneBuilder {
-    fn group_by(self, key: DuneBaseAggType) -> Self {
-        self
-    }
-    fn paged(self) -> Self {
-        panic!("Cannot page a paged builder");
-    }
-    fn overview(self) -> Self {
-        self
-    }
-    fn posts(self) -> Self {
-        self
-    }
-}
-
-impl<'a> DuneBuilderConsumable for PageDuneBuilder<'a> {
-    fn consume(self) -> Result<(), Box<Error>> {
-        Ok(())
-    }
-}
-
-struct GroupedDuneBuilder;
-
-impl GroupedDuneBuilder {
-    fn with<F>(self, action: F) -> Self where F: (Fn(Builder) -> Builder) {
-    }
-}
-
-impl<'a> DuneBuilderConsumable for GroupedDuneBuilder<'a> {
-    fn consume(self) -> Result<(), Box<Error>> {
-        Ok(())
-    }
-}
-
-#[test]
-fn testing() {
-    let start = Builder {};
-    start.group_by(DuneBaseAggType::Day)
-        .overview()
-        .with(|builder| {
-            builder.group_by(DuneBaseAggType::Year)
-                .overview()
-        })
-        .build();
-}
-*/
