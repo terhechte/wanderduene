6e05cf9c95c42c771bc4b1caf10ea55ba33404c9
Benedikt Terhechte <terhechte@gmail.com>
2018-04-23 21:17:00 +0200

    Compiles now with templates.

--- src/html_writer.rs
+++ src/html_writer.rs
@@ -3,6 +3,7 @@ use askama::Template;
 use std::io;
 use std::rc::Rc;
 use std::path::{Path, PathBuf};
+use std::marker::PhantomData;
 
 // FIXME:
 use std::error::Error;
@@ -21,17 +22,17 @@ pub struct HTMLWriter {
 /// All the shared structures are part of this base html.
 #[derive(Template)]
 #[template(path = "base.html")]
-struct BaseTemplate<'a, Router> where Router: 'a {
+struct BaseTemplate<'a, Router> where Router: 'a + DuneRouter {
     base: &'a Rc<DuneBase>,
     config: &'a Rc<Configuration>,
-    router: &'a Router
+    router: RouterWraper<Router>
 }
 
 /// This template is used for rendering an Index
 /// I.e. a list of posts where each post contains the full post
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate<'a, Router> where Router: 'a {
+struct IndexTemplate<'a, Router> where Router: 'a + DuneRouter {
     pagination: &'a Option<DunePagination>,
     posts: &'a Vec<DunePost>,
     _parent: BaseTemplate<'a, Router>
@@ -40,7 +41,7 @@ struct IndexTemplate<'a, Router> where Router: 'a {
 /// I.e. a list of posts where each post is only the headline
 #[derive(Template)]
 #[template(path = "overview.html")]
-struct OverviewTemplate<'a, Router> where Router: 'a {
+struct OverviewTemplate<'a, Router> where Router: 'a + DuneRouter {
     pagination: &'a Option<DunePagination>,
     posts: &'a Vec<DunePost>,
     _parent: BaseTemplate<'a, Router>
@@ -50,13 +51,28 @@ struct OverviewTemplate<'a, Router> where Router: 'a {
 /// I.e. a single blog post
 #[derive(Template)]
 #[template(path = "post.html")]
-struct PostTemplate<'a, Router> where Router: 'a {
+struct PostTemplate<'a, Router> where Router: 'a + DuneRouter {
     pagination: &'a Option<DunePagination>,
     post: &'a DunePost,
     _parent: BaseTemplate<'a, Router>
 }
 
 // MOVE ROUTER INTO A NEWTYPE WRAPPRE?
+struct RouterWraper<T: DuneRouter> {
+    __router: PhantomData<T>
+}
+
+impl<T> RouterWraper<T> where T: DuneRouter {
+    fn post(&self, post: &DunePost) -> String {
+        T::post(post)
+    }
+    fn tag(&self, tag: &str) -> String {
+        "".to_string()
+    }
+    fn keyword(&self, keyword: &str) -> String {
+        "".to_string()
+    }
+}
 
 impl HTMLWriter {
     pub fn new(configuration: Rc<Configuration>) -> HTMLWriter {
@@ -69,7 +85,9 @@ impl HTMLWriter {
         BaseTemplate {
             base: base,
             config: &self.configuration,
-            router: router
+            router: RouterWraper {
+                __router: PhantomData::<Router>
+            }
         }
     }
 
